08e505a6db83826aea69f0f4a703a83cae6cbaee
ntorresd <ntorresd@unal.edu.co>
2022-11-09 09:41:55 -0500

    fix: updated paths and cleaned variants_recod. I am still not able to use config.json in the current format for R scripts.

--- genomics/scripts/variants_recod.R
+++ genomics/scripts/variants_recod.R
@@ -1,23 +1,24 @@
 rm(list= ls()) ## Borra los objetos anteriores
 
-library(rstudioapi) 
-setwd(dirname(getActiveDocumentContext()$path))
+library(configr)
+library(here)
 
+#### PATHS ####
+setwd(here())
+DATA_PATH = "data/"
+OUT_PATH = "genomics/outputs/"
 
 #### LOAD FUNCTIONS ####
-
-source("../functions/genomics_functions.R")
+source("genomics/scripts/genomics_functions.R")
 `%!in%` <- Negate(`%in%`)
 
-#### DATES AND PATHS ####
-download_date <-  '2022-08-17'
-download_date_ <- gsub("-","",download_date)
 
+#### DATES ####
 initial_date = as.Date("2021-03-22") 
 
 #### READ DATA ####
 
-df_genomes_bog <- read_csv('../../data/genomes_bog_raw.csv')
+df_genomes_bog <- read_csv(paste0(DATA_PATH,'genomes_bog_raw.csv'))
 
 df_genomes_bog$date <- df_genomes_bog$date %>% as.Date()
 df_genomes_bog <- df_genomes_bog[df_genomes_bog$date >= initial_date, ]
@@ -28,14 +29,14 @@ dic_var_bog <- generate_dictionary(df_genomes_bog)
 rm(list = ls(pat = "^filtro"))
 df_genomes_bog <- lineage_codification(df_genomas = df_genomes_bog, dic_variants=dic_var_bog)
 
-#### HISTOGRAMA VARIANTS BOGOTÁ ####
+#### HISTOGRAM VARIANTS BOGOTÁ ####
 
 df_grouped <- group_variant_epiweek(df_genomas_recod = df_genomes_bog, initial_date = initial_date)
 df_grouped <- df_grouped %>% 
   mutate(lineage=factor(lineage, levels = c("Alpha", "Gamma", "Mu", "Other", "Delta", "Omicron"))) %>% 
   arrange(date, lineage)
 
-write.csv(df_grouped, paste0("../outputs/variants-ic-bog_", download_date_,".csv"), row.names = FALSE)
+write.csv(df_grouped, paste0(OUT_PATH, "/variants-ic-bog.csv"), row.names = FALSE)
 
 unique(df_genomes_bog[(df_genomes_bog$lineage_recod=="Other") & (df_genomes_bog$date >="2021-12-16"),]$lineage)
 unique(df_genomes_bog[(df_genomes_bog$lineage_recod=="Omicron"),]$lineage)
