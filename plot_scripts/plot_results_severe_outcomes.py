1d2c436514516a1c07822492a2b1e46f94cc5e65
ntorresd <ntorresd@unal.edu.co>
2022-11-23 16:26:54 -0500

    feat: add necessary functions to plot the stacked histogram of cases by wave and age group to plot_scripts/plot_results_severe_outcomes.py. The corresponding figure is stored in severe_outcomes/figures/hosp_icu_death_counts_hist.png. The histogram plot requires the installation of the metbrewer package to generate the color pallete used (https://github.com/BlakeRMills/MetBrewer). I added this to requirements.txt as a comment.

--- plot_scripts/plot_results_severe_outcomes.py
+++ plot_scripts/plot_results_severe_outcomes.py
@@ -6,10 +6,10 @@ Adapted from: https://github.com/mrc-ide/Brazil_COVID19_distributions
 @author: dsquevedo
 @author: ntorresd
 """
-
-import sys
+# %%
 import yaml
 import pandas as pd
+import numpy as np
 import datetime as dt
 import matplotlib.pyplot as plt
 
@@ -44,73 +44,121 @@ def plot_xyvar(df, ax, n_strat, varx='age_group', vary='percentage', title=None)
     ax.plot(data[varx], data[vary], label=strat+str(n_strat), linestyle='-', marker='o')
     ax.set_title(title)
 
-# Percentages
-wave_list = df_hosp_perc['wave'].unique()
-fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+# Wave cases percentage distribution by age group
+def plot_percentage():
+    vary = 'percentage'
+    wave_list = df_hosp_perc['wave'].unique()
 
-vary = 'percentage'
-for axi in ax:
-    axi.tick_params(axis='x', labelrotation=90)
-    axi.set_ylabel(vary)
-    axi.set_xlabel('age group')
+    fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+    for axi in ax:
+        axi.tick_params(axis='x', labelrotation=90)
+        axi.set_ylabel(vary)
+        axi.set_xlabel('age group')
 
-for wave in wave_list:
-    plot_xyvar(df_hosp_perc, ax=ax[0], n_strat=wave, vary=vary, title='a.')
-    plot_xyvar(df_icu_perc, ax=ax[1], n_strat=wave, vary=vary, title='b.')
-    plot_xyvar(df_death_perc, ax=ax[2], n_strat=wave, vary=vary, title='c.')
+    for wave in wave_list:
+        plot_xyvar(df_hosp_perc, ax=ax[0], n_strat=wave, vary=vary, title='a.')
+        plot_xyvar(df_icu_perc, ax=ax[1], n_strat=wave, vary=vary, title='b.')
+        plot_xyvar(df_death_perc, ax=ax[2], n_strat=wave, vary=vary, title='c.')
 
-handles, labels = ax[0].get_legend_handles_labels()
-fig.legend(handles, labels, bbox_to_anchor = (0.8, -0.03), ncol = len(wave_list))    
-fig.show()
-fig.savefig(FIG_PATH+'hosp_icu_death_percentages.png')
+    handles, labels = ax[0].get_legend_handles_labels()
+    fig.legend(handles, labels, bbox_to_anchor = (0.8, -0.03), ncol = len(wave_list))    
+    fig.savefig(FIG_PATH+'hosp_icu_death_percentages.png')
+    return fig, ax
 
-# Counts
-wave_list = df_hosp_perc['wave'].unique()
-fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+# %%
+# Wave counts by age group
+def plot_counts():
+    wave_list = df_hosp_perc['wave'].unique()
+    fig, ax = plt.subplots(1, 3, figsize=(15, 5))
 
-vary = 'counts'
-for axi in ax:
-    axi.tick_params(axis='x', labelrotation=90)
-    axi.set_ylabel(vary)
-    axi.set_xlabel('age group')
+    vary = 'counts'
+    for axi in ax:
+        axi.tick_params(axis='x', labelrotation=90)
+        axi.set_ylabel(vary)
+        axi.set_xlabel('age group')
 
-for wave in wave_list:
-    plot_xyvar(df_hosp_perc, ax=ax[0], n_strat=wave, vary=vary, title='a.')
-    plot_xyvar(df_icu_perc, ax=ax[1], n_strat=wave, vary=vary, title='b.')
-    plot_xyvar(df_death_perc, ax=ax[2], n_strat=wave, vary=vary, title='c.')
+    for wave in wave_list:
+        plot_xyvar(df_hosp_perc, ax=ax[0], n_strat=wave, vary=vary, title='a.')
+        plot_xyvar(df_icu_perc, ax=ax[1], n_strat=wave, vary=vary, title='b.')
+        plot_xyvar(df_death_perc, ax=ax[2], n_strat=wave, vary=vary, title='c.')
 
-handles, labels = ax[0].get_legend_handles_labels()
-fig.legend(handles, labels, bbox_to_anchor = (0.8, -0.03), ncol = len(wave_list))    
-fig.show()
-fig.savefig(FIG_PATH+'hosp_icu_death_counts.png')
+    handles, labels = ax[0].get_legend_handles_labels()
+    fig.legend(handles, labels, bbox_to_anchor = (0.8, -0.03), ncol = len(wave_list))    
+    fig.savefig(FIG_PATH+'hosp_icu_death_counts.png')
+    return fig, ax
+
+# %%
+
+# Stacked histogram of cases by wave and age group
+from met_brewer import met_brew
+#  Package installation : https://github.com/BlakeRMills/MetBrewer
+
+def plot_stacked(df, ax, strat='wave', group_var='age_group', vary='counts', pallete='VanGogh1', title=None): 
+    strat_list = df[strat].unique()
+    group_list = df[group_var].unique()
+    color_list = met_brew(pallete, n=len(group_list), brew_type='continuous')
+    
+    y_offset = np.zeros(len(strat_list))
+    counter=0
+    for group in group_list:
+        mask = (df[group_var]==group)
+        counts_list =  df.loc[mask][vary].values
+        ax.bar(strat_list, counts_list, label=group, bottom=y_offset, color=color_list[counter]) 
+        y_offset = y_offset + counts_list 
+        counter+=1
+    ax.set_title(title)
+
+def plot_counts_hist():
+    fig, ax = plt.subplots(1, 3, figsize=(15, 5), sharey=False)
+
+    strat = 'wave'
+    vary = 'counts'
+    plot_stacked(df = df_hosp_perc, ax=ax[0], title='a.')
+    plot_stacked(df = df_icu_perc, ax=ax[1], title='b.')
+    plot_stacked(df = df_death_perc, ax=ax[2], title='c.')
+
+    for axi in ax:
+        axi.set_ylabel(vary)
+        axi.set_xlabel(strat)
+
+    handles, labels = axi.get_legend_handles_labels()
+    fig.legend(handles, labels, bbox_to_anchor = (0.95, -0.03), ncol = len(labels))   
+    fig.savefig(FIG_PATH+'hosp_icu_death_counts_hist.png') 
+
+    return fig, ax
+
+plot_percentage()
+# plot_counts()
+plot_counts_hist()
 
 # Percentages and counts
-wave_list = df_hosp_perc['wave'].unique()
-fig, ax = plt.subplots(2, 3, figsize=(15, 10))
+# wave_list = df_hosp_perc['wave'].unique()
+# fig, ax = plt.subplots(2, 3, figsize=(15, 10))
 
-vary = 'percentage'
-for axi in ax[0]:
-    axi.tick_params(axis='x', labelrotation=90)
-    axi.set_ylabel(vary)
-    axi.set_xlabel('age group')
+# vary = 'percentage'
+# for axi in ax[0]:
+#     axi.tick_params(axis='x', labelrotation=90)
+#     axi.set_ylabel(vary)
+#     axi.set_xlabel('age group')
 
-for wave in wave_list:
-    plot_xyvar(df_hosp_perc, ax=ax[0][0], n_strat=wave, vary=vary, title='a.')
-    plot_xyvar(df_icu_perc, ax=ax[0][1], n_strat=wave, vary=vary, title='b.')
-    plot_xyvar(df_death_perc, ax=ax[0][2], n_strat=wave, vary=vary, title='c.')
+# for wave in wave_list:
+#     plot_xyvar(df_hosp_perc, ax=ax[0][0], n_strat=wave, vary=vary, title='a.')
+#     plot_xyvar(df_icu_perc, ax=ax[0][1], n_strat=wave, vary=vary, title='b.')
+#     plot_xyvar(df_death_perc, ax=ax[0][2], n_strat=wave, vary=vary, title='c.')
 
-vary = 'counts'
-for axi in ax[1]:
-    axi.tick_params(axis='x', labelrotation=90)
-    axi.set_ylabel(vary)
-    axi.set_xlabel('age group')
+# vary = 'counts'
+# for axi in ax[1]:
+#     axi.tick_params(axis='x', labelrotation=90)
+#     axi.set_ylabel(vary)
+#     axi.set_xlabel('age group')
 
-for wave in wave_list:
-    plot_xyvar(df_hosp_perc, ax=ax[1][0], n_strat=wave, vary=vary, title='d.')
-    plot_xyvar(df_icu_perc, ax=ax[1][1], n_strat=wave, vary=vary, title='e.')
-    plot_xyvar(df_death_perc, ax=ax[1][2], n_strat=wave, vary=vary, title='f.')    
+# for wave in wave_list:
+#     plot_xyvar(df_hosp_perc, ax=ax[1][0], n_strat=wave, vary=vary, title='d.')
+#     plot_xyvar(df_icu_perc, ax=ax[1][1], n_strat=wave, vary=vary, title='e.')
+#     plot_xyvar(df_death_perc, ax=ax[1][2], n_strat=wave, vary=vary, title='f.')    
     
-handles, labels = ax[0][0].get_legend_handles_labels()
-fig.legend(handles, labels, bbox_to_anchor = (0.8, -0.03), ncol = len(wave_list))    
-fig.show()
-fig.savefig(FIG_PATH+'hosp_icu_death_percentages_counts.png')
+# handles, labels = ax[0][0].get_legend_handles_labels()
+# fig.legend(handles, labels, bbox_to_anchor = (0.8, -0.03), ncol = len(wave_list))    
+# fig.show()
+# fig.savefig(FIG_PATH+'hosp_icu_death_percentages_counts.png')
+# %%
