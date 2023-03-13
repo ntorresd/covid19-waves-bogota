ec1af50f1d8c22bcd4016c68f0bf707ff0ad029b
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2023-03-13 21:13:02 +0100

    fix: start date of first wave. Using 1st march 2020 now

--- waves/scripts/process_waves.py
+++ waves/scripts/process_waves.py
@@ -16,11 +16,14 @@ config = cfg["default"]
 OUT_PATH = config['PATHS']['OUT_PATH'].format(dir = 'waves')
 # The roots were selected by visual inspection of /figures/roots_confirmed_cases.png 
 # and set by default in config.yml
+# We set 1st of March of 2020 as the default start date for the first wave
 DATES = config['WAVES']['ROOTS'] 
 
 df_roots = pd.read_csv(OUT_PATH + 'roots_confirmed_cases.csv')
 
-df_waves = df_roots.iloc[DATES]
+df_waves = pd.DataFrame({'date': [pd.to_datetime('2020-03-01')]})
+df_waves = pd.concat([df_waves, df_roots.iloc[DATES]])
+df_waves['date'] = pd.to_datetime(df_waves['date']).dt.date
 df_waves['wave'] = [1, 1, 2, 2, 3, 3, 4, 4]
 df_waves['label'] = ['start', 'end', 'start', 'end', 'start', 'end', 'start', 'end']
 df_waves_t = df_waves.pivot(index = 'wave', columns = 'label', values = 'date').reset_index()
